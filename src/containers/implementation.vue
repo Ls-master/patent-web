e8cd35129010a0f8c72b9f20de4a7e3f4a82165f
Ls <15376769536@163.com>
2018-06-12 10:28:00 +0800

    finash layout implementation

--- src/containers/implementation.vue
+++ src/containers/implementation.vue
@@ -1,16 +1,112 @@
 <template lang="html">
   <div class="containers-implementation">
-    implementation
+    <div class="imp-title">
+      具体实施方法
+    </div>
+    <div class="imp-table">
+      <el-table
+        :data="tableData"
+        height="100%"
+        border
+        style="width: 100%">
+        <el-table-column
+          type="index"
+          width="50">
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="手段"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="功能"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="效果">
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-08',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-07',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      },{
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-08',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-07',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }]
+    }
+  }
 }
 </script>
 
 <style lang="scss">
   .containers-implementation {
-
+    height: 100%;
+    .imp-title {
+      margin-bottom: 20px;
+      font-size: 20px;
+    }
+    .imp-table {
+      height: calc(100% - 40px);
+    }
   }
 </style>
